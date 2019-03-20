e22b5d63063b9f14eb06dc4d156b7201074b6efe
Kevin O'Brien <kpobrien@buffalo.edu>
2019-03-20 22:10:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+if phrase == "".upcase
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "".downcase
+  puts "NO, NOT SINCE 1938!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
