a0756c38e879d223fb6c41b8acf4cfa195d2ffcb
Kevin O'Brien <kpobrien@buffalo.edu>
2019-03-20 22:24:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
+    elsif phrase == phrase.capitalize
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
